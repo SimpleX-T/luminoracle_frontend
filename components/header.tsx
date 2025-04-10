c16c91c1f6e6d3057ecf4d20fa028737f4fd8ca6
SimpleX-T <ntmark2004@gmail.com>
2025-04-10 21:02:45 +0100

    fixed issue on mobile

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FaXTwitter } from "react-icons/fa6";
+import { FaGoogle, FaXTwitter } from "react-icons/fa6";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import Image from "next/image";
@@ -8,7 +8,10 @@ export default function Header() {
   return (
     <header className="w-full border-b border-border/40 bg-background/80 backdrop-blur-sm fixed z-50 top-0">
       <div className="container flex h-14 items-center justify-between">
-        <Link className="flex items-center gap-2 w-12 h-12" href="/">
+        <Link
+          className="flex items-center gap-2 w-8 md:w-12 h-8 md:h-12"
+          href="/"
+        >
           <img
             src="/logo.png"
             alt="lumin"
@@ -25,7 +28,7 @@ export default function Header() {
             <Image
               width={150}
               height={150}
-              src="https://www.seekeroracle.com/dexscreener_logo.svg"
+              src="/dexscreener_logo.svg"
               alt="dexscreener"
               className="w-full h-full object-cover group-hover:invert transition-colors duration-300"
             />
@@ -38,22 +41,15 @@ export default function Header() {
           >
             <FaXTwitter size={20} />
           </Link>
-          <Button variant="outline" size="sm">
+          <Button
+            variant="outline"
+            size="sm"
+            className="hidden md:inline-block"
+          >
             Email Login
           </Button>
-          <Button variant="outline" size="sm" className="gap-1">
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.057-1.036-.202-1.625z"
-                fill="currentColor"
-              />
-            </svg>
+          <Button variant="outline" size="sm" className="gap-1 hidden md:flex">
+            <FaGoogle />
             Login with Google
           </Button>
           {/* <ThemeToggle /> */}
